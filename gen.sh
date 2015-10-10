29e98fe1d71fc2533569a4aee3f0303e6a5aa19f
Juanjo Conti <jjconti@carouselapps.com>
2015-10-10 00:29:46 -0300

    Tidy and add title

--- gen.sh
+++ gen.sh
@@ -1,3 +1,6 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-rst2html5 --jquery --reveal-js --reveal-js-opts theme=black,transition=convex --pretty-print-code --pretty-print-code-opts langs=clj --stylesheet-path=slides.css missing-python.rst > missing-python-reveal.html
+rst2html5 --jquery --reveal-js --reveal-js-opts theme=black,transition=convex \
+          --pretty-print-code --pretty-print-code-opts langs=clj \
+          --stylesheet-path=slides.css missing-python.rst \
+          --title "¿Qué extraño de Python?" > missing-python-reveal.html
